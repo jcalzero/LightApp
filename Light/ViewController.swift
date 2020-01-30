7cbdad63138d00197c37e745bb6dd3b843fb6e01
JULIEN CALFAYAN <5005236270@mwce111-01.local>
2020-01-30 15:20:34 -0800

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by JULIEN CALFAYAN on 1/30/20.
+//  Copyright © 2020 JULIEN CALFAYAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
