00563853b6117ad25706b6f4420e24032be68e0b
JULIEN CALFAYAN <5005236270@mwce111-01.local>
2020-01-30 15:37:40 -0800

    Done

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        lightButton.setTitle("Dark", for: .normal)
+        lightButton.setTitleColor(.black, for: .normal)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onLightButtonClick(_ sender: Any) {
+        if lightButton.titleLabel?.text == "Dark" {
+            lightButton.setTitle("Light", for: .normal)
+            lightButton.setTitleColor(.white, for: .normal)
+            self.view.backgroundColor = .black
+        } else {
+            lightButton.setTitle("Dark", for: .normal)
+            lightButton.setTitleColor(.black, for: .normal)
+            self.view.backgroundColor = .white
+        }
+    }
 }
 
