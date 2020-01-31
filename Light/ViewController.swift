d2e4682e4297a833df0406791aaf28296857e82b
JULIEN CALFAYAN <5005236270@mwce111-01.local>
2020-01-30 16:45:30 -0800

    Done

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -11,24 +11,42 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var lightButton: UIButton!
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
-        lightButton.setTitle("Dark", for: .normal)
-        lightButton.setTitleColor(.black, for: .normal)
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        lightButton.layer.borderWidth = 1.0
+        lightButton.layer.borderColor = UIColor.white.cgColor
+        lightButton.layer.cornerRadius = lightButton.frame.width / 2
+        lightButton.layer.cornerRadius = lightButton.frame.width / 2
+        lightButton.layer.masksToBounds = true
+        lightButton.setTitle("Light", for: .normal)
+        lightButton.setTitleColor(.white, for: .normal)
+        
+        self.view.backgroundColor = .black
     }
 
     @IBAction func onLightButtonClick(_ sender: Any) {
         if lightButton.titleLabel?.text == "Dark" {
+            lightButton.layer.borderWidth = 1.0
+            lightButton.layer.borderColor = UIColor.white.cgColor
+            lightButton.layer.cornerRadius = lightButton.frame.width / 2
+            lightButton.layer.masksToBounds = true
             lightButton.setTitle("Light", for: .normal)
             lightButton.setTitleColor(.white, for: .normal)
             self.view.backgroundColor = .black
         } else {
+            lightButton.layer.borderWidth = 1.0
+            lightButton.layer.borderColor = UIColor.black.cgColor
+            lightButton.layer.cornerRadius = lightButton.frame.width / 2
+            lightButton.layer.masksToBounds = true
             lightButton.setTitle("Dark", for: .normal)
             lightButton.setTitleColor(.black, for: .normal)
+            
             self.view.backgroundColor = .white
         }
     }
 }
-
